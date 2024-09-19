afc2bb067da7e6306cfee6d39ff23686885e0d17
Arthur Santos Marinho de Souza <arthur.souza2005@hotmail.com>
2024-09-19 19:12:22 -0300

    Metrics alongside dataframe

--- Stock_Portfoliio_Dataframe.py
+++ Stock_Portfoliio_Dataframe.py
@@ -6,7 +6,8 @@ def generate_summarized_visualization(portfolio_dataframe):
     filtered_dataframe = portfolio_dataframe[portfolio_dataframe['Sell Date'].isna()]
 
     # Select only the columns 'a', 'b', and 'c'
-    filtered_dataframe = filtered_dataframe[['Days Holding', 'ROI', 'Sector', 'Market Cap', 'Total Amount', 'Investment']]
+    filtered_dataframe = filtered_dataframe[
+        ['Days Holding', 'ROI', 'Sector', 'Market Cap', 'Total Amount', 'Investment']]
 
     # Tabela de Portfólio no topo
     st.header(f'Stock Portfolio', divider='gray')
@@ -15,27 +16,26 @@ def generate_summarized_visualization(portfolio_dataframe):
         color = 'red' if value < 0 else 'green'
         return f'color: {color}'
 
-    # Apply coloring and formatting
-    colored_portfolio = filtered_dataframe.style.applymap(color_negative_red, subset=['ROI'])
-    colored_portfolio = colored_portfolio.format({'ROI': '{:.2f}%'})
-    colored_portfolio = colored_portfolio.format({'Allocation': '{:.2f}'})
+    col1, col2 = st.columns(2)
+    with col1:  # Apply coloring and formatting
+        colored_portfolio = filtered_dataframe.style.applymap(color_negative_red, subset=['ROI'])
+        colored_portfolio = colored_portfolio.format({'ROI': '{:.2f}%'})
+        colored_portfolio = colored_portfolio.format({'Allocation': '{:.2f}'})
 
-    # Display dataframe
-    st.dataframe(data=colored_portfolio, height=300)
-
-    col1, col2, col3 = st.columns(3)
-
-    sum_amount = int(filtered_dataframe["Total Amount"].sum())
-    sum_investment = int(filtered_dataframe["Investment"].sum())
-    percentage_return = ((sum_amount / sum_investment) - 1) * 100
-
-    with col1:
-        st.metric(label=f'Total Amount', value=sum_amount)
+        # Display dataframe
+        st.dataframe(data=colored_portfolio, height=300)
 
     with col2:
+        col1, col2, col3 = st.columns(3)
+
+        sum_amount = int(filtered_dataframe["Total Amount"].sum())
+        sum_investment = int(filtered_dataframe["Investment"].sum())
+        percentage_return = ((sum_amount / sum_investment) - 1) * 100
+
+        st.metric(label=f'Total Amount', value=sum_amount)
+
         st.metric(label=f'Total Investment', value=sum_investment)
 
-    with col3:
         st.metric(label=f'Total Return', value=f'{percentage_return:.2f}%')
 
 
@@ -43,11 +43,9 @@ def generate_dataframe_visualization(portfolio_dataframe):
     # Tabela de Portfólio no topo
     st.header(f'Stock Portfolio', divider='gray')
 
-
     def color_negative_red(value):
         color = 'red' if value < 0 else 'green'
         return f'color: {color}'
-
 
     colored_portfolio = portfolio_dataframe.style.applymap(color_negative_red, subset=['ROI'])
     colored_portfolio = colored_portfolio.format({'ROI': '{:.2f}%'})
